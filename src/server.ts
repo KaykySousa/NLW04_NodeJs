73e95ca33273c807eb6002de45d8f1a9a810db9c
Kayky de Sousa <kaykysousa321@gmail.com>
2021-02-26 21:11:36 -0300

    refactored server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,6 @@
-import 'reflect-metadata'
-import express from 'express'
-import "./database"
-import { router } from './routes'
+import { app } from "./app"
 
-const app = express()
 const port = 4000
-
-app.use(express.json())
-app.use(router)
-
 app.listen(port, () => {
     console.log(`The server is running on 127.0.0.1:${port} !`)
 })
