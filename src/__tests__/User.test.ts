b35cc4168c98338bc7d15376ca1a0daaa5b00851
Kayky de Sousa <kaykysousa321@gmail.com>
2021-02-26 21:20:14 -0300

    added automated tests with Jest

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest'
+import { app } from '../app'
+
+import createConnection from '../database/index'
+
+describe("Users", () => {
+    beforeAll(async () => {
+        const connection = await createConnection()
+        await connection.runMigrations()
+    })
+
+    it("Should be able to create a new user", async () => {
+        const response = await request(app).post("/users").send({
+            email: "Aubree.Legros@example.net",
+            name: "Aubree.Legros"
+        })
+
+        expect(response.status).toBe(201)
+    })
+
+    it("Shouldn't be able to create a user width exists email", async () => {
+        const response = await request(app).post("/users").send({
+            email: "Aubree.Legros@example.net",
+            name: "Aubree.Legros"
+        })
+
+        expect(response.status).toBe(400)
+    })
+
+})
